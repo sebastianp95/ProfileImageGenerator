fb4a3363a73b651cdab944e39d4ac6d37c02a4d7
J. Sebastian Perez <sebastian_perez95@hotmail.com>
2024-12-27 19:14:45 -0500

    new

--- frontend/profileImgGenerator/vite.config.js
+++ frontend/profileImgGenerator/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ProfileImageGenerator/', // Add the base path for GitHub Pages
+  base: "/ProfileImageGenerator/frontend/profileImgGenerator/", // Add the base path for GitHub Pages
   plugins: [react()],
 });
